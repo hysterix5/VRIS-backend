838169169085364f2b746b1fcb6e68b391f5d925
Joseph <joseph.limbaroc@maxias.net>
2024-03-11 15:16:40 +0800

    Update

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
+use Faker\Factory as Faker;
 
 class UserSeeder extends Seeder
 {
@@ -13,11 +14,22 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
+        $faker = Faker::create();
+
         User::create([
             'firstname' => 'John',
             'lastname' => 'Doe',
             'email' => 'user@test.com',
             'password' => Hash::make('password'),
         ]);
+
+        for ($i = 0; $i < 99; $i++) {
+            User::create([
+                'firstname' => $faker->firstName(),
+                'lastname' => $faker->lastName(),
+                'email' => $faker->email(),
+                'password' => Hash::make('password')
+            ]);
+        }
     }
 }
